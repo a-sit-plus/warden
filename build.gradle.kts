44c21361bf1dd5efa8aa915fa23048c9c07a0adf
Bernd Prünster <bernd.pruenster@a-sit.at>
2024-10-04 19:46:08 +0200

    work around nexus publishing bug

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,8 @@
 plugins {  id("at.asitplus.gradle.conventions") version "2.0.20+20240920" }
 
 group = "at.asitplus"
+
+//work around nexus publish bug
+val artifactVersion: String by extra
+version = artifactVersion
+//end work around nexus publish bug
